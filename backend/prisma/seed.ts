906273cefa9c986d08cb89c2f36c142cb5a0083d
de-scientist <gitaumark502@gmail.com>
2026-01-02 09:02:47 +0300

    update the seed.ts by adding the datasource Url

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+})
+
 
 async function main() {
   console.log('🌱 Seeding database...')
