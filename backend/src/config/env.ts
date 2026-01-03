b6d0a7fc9430e5de52d21cc311ca883f79c6d33c
de-scientist <gitaumark502@gmail.com>
2026-01-03 14:29:05 +0300

    updated the env.ts file

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,16 +1,26 @@
 import dotenv from 'dotenv'
 
 dotenv.config()
+const port = process.env.PORT || 5000
+const nodeEnv = process.env.NODE_ENV || 'development'
+export const jwtSecret = process.env.JWT_SECRET || 'fallback-secret-change-in-production'
+export const jwtExpire = process.env.JWT_EXPIRE || '7d'
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173'
+const databaseUrl = process.env.DATABASE_URL
+const adminEmail = process.env.ADMIN_EMAIL || 'admin@mutcu.ac.ke'
+const adminPassword = process.env.ADMIN_PASSWORD || 'admin123'
 
-// export default {
-//   port: process.env.PORT || 5000,
-//   nodeEnv: process.env.NODE_ENV || 'development',
-//   jwtSecret: process.env.JWT_SECRET || 'fallback-secret-change-in-production',
-//   jwtExpire: process.env.JWT_EXPIRE || '7d',
-//   frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173',
-//   databaseUrl: process.env.DATABASE_URL,
-//   adminEmail: process.env.ADMIN_EMAIL || 'admin@mutcu.ac.ke',
-//   adminPassword: process.env.ADMIN_PASSWORD || 'admin123',
-// }
+const env = {
+	port,
+	nodeEnv,
+	jwtSecret,
+	jwtExpire,
+	frontendUrl,
+	databaseUrl,
+	adminEmail,
+	adminPassword,
+}
+
+export default env
 
 
