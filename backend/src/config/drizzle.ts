a3d3b2500e642e906cde4e8bb6bf7bcf4c8be3cc
de-scientist <gitaumark502@gmail.com>
2026-01-03 14:15:27 +0300

    update drizzle

--- backend/src/config/drizzle.ts
+++ backend/src/config/drizzle.ts
@@ -4,9 +4,11 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 
 dotenv.config()
 
+
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 
 export const db = drizzle(pool)
+export { pool }
 
 process.on('beforeExit', async () => {
   await pool.end()
