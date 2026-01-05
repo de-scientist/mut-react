a33c1077a874a3288629cc299ea5ea84cd0db204
de-scientist <gitaumark502@gmail.com>
2026-01-05 18:23:40 +0300

    update import { useState } from 'react' import type { FormEvent } from 'react'

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
+import type { FormEvent } from 'react'
 import { Link } from 'react-router-dom'
 import '../assets/mut/css/index.css'
 import ConfirmationModal from '../components/ConfirmationModal'
