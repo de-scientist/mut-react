1b49575c299d2a470de4a43bbf4f2250f6a8a3b9
de-scientist <gitaumark502@gmail.com>
2026-01-08 16:15:14 +0300

    update media routes

--- backend/src/routes/mediaRoutes.ts
+++ backend/src/routes/mediaRoutes.ts
@@ -1,12 +1,20 @@
 import express from 'express'
-import { createGalleryItem, getGallery, updateGalleryItem, getAllGalleryAdmin } from '../modules/media/mediaController.ts'
+import { createGalleryItem, getGallery, updateGalleryItem, getAllGalleryAdmin, deleteGalleryItem } from '../modules/media/mediaController.ts'
 
 
 const router = express.Router()
 
 router.get('/public', getGallery) // public gallery
+
+// All routes protected for admin
+router.use(authenticate)
+router.use(requireAdmin)
+
+// Admin
+router.post('/admin', createGalleryItem)
+router.put('/admin/:id', updateGalleryItem)
+router.delete('/admin/:id', deleteGalleryItem)
 router.get('/admin', getAllGalleryAdmin)
-router.post('/admin', upload.single('image'), createGalleryItem)
-router.put('/admin/:id', upload.single('image'), updateGalleryItem)
+
 
 export default router
