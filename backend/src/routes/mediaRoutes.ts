ca5573e8d4c03527346f5d962532fd8d0da97f7b
de-scientist <gitaumark502@gmail.com>
2026-01-08 19:03:01 +0300

    update the mediaRoutes

--- backend/src/routes/mediaRoutes.ts
+++ backend/src/routes/mediaRoutes.ts
@@ -1,22 +1,31 @@
 import express from 'express'
-import { createGalleryItem, getGallery, updateGalleryItem, getAllGalleryAdmin, deleteGalleryItem, toggleGalleryItem } from '../modules/media/mediaController.ts'
+import {
+  createGalleryItem,
+  getGallery,
+  updateGalleryItem,
+  getAllGalleryAdmin,
+  deleteGalleryItem,
+  toggleGalleryItem,
+} from '../modules/media/mediaController.js'
 import { authenticate, requireAdmin } from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.get('/public', getGallery) // public gallery
+/* =======================
+   PUBLIC
+======================= */
+router.get('/public', getGallery)
 
-// All routes protected for admin
+/* =======================
+   ADMIN
+======================= */
 router.use(authenticate)
 router.use(requireAdmin)
 
-// Admin
 router.get('/admin', getAllGalleryAdmin)
 router.post('/admin', createGalleryItem)
 router.put('/admin/:id', updateGalleryItem)
 router.patch('/admin/:id/toggle', toggleGalleryItem)
 router.delete('/admin/:id', deleteGalleryItem)
-
-
 
 export default router
