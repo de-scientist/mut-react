c1301a1828c505a8db42ddb24152c7804c5011a2
de-scientist <gitaumark502@gmail.com>
2026-01-08 16:18:30 +0300

    add the toggleGalleryItem import

--- backend/src/routes/mediaRoutes.ts
+++ backend/src/routes/mediaRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createGalleryItem, getGallery, updateGalleryItem, getAllGalleryAdmin, deleteGalleryItem } from '../modules/media/mediaController.ts'
+import { createGalleryItem, getGallery, updateGalleryItem, getAllGalleryAdmin, deleteGalleryItem, toggleGalleryItem } from '../modules/media/mediaController.ts'
 import { authenticate, requireAdmin } from '../middlewares/auth.js'
 
 const router = express.Router()
