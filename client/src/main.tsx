6417e6a245c3296d43a277eda77b4106ed3b3543
Mark Kinyanjui <gitaumark502@gmail.com>
2026-01-22 13:20:40 +0300

    update: import "bootstrap-icons/font/bootstrap-icons.min.css";

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
